380f1e4ccfd49913220783951ade10ec7806b6db
liaskas@gmail.com <Vagelis@Vagelis-iMac.local>
2011-06-22 00:20:56 +0300

    Yet more Testing. New Group and files

---
+++ testProject/testProject/dummyViewController.h
@@ -0,0 +1,16 @@
+//
+//  dummyViewController.h
+//  testProject
+//
+//  Created by EYAGGELOS LIASKAS on 22/06/2011.
+//  Copyright 2011 Desquared. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface dummyViewController : UIViewController {
+    
+}
+
+@end
---
+++ testProject/testProject/dummyViewController.m
@@ -0,0 +1,57 @@
+//
+//  dummyViewController.m
+//  testProject
+//
+//  Created by EYAGGELOS LIASKAS on 22/06/2011.
+//  Copyright 2011 Desquared. All rights reserved.
+//
+
+#import "dummyViewController.h"
+
+
+@implementation dummyViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
